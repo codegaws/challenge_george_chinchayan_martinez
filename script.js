a05ad479ea827252afbc5506808d16f433ef84c0
codegaws <codegaws@gmail.com>
2024-03-02 21:16:05 -0500

    faltan algunos arreglos en css y JS

--- script.js
+++ script.js
@@ -1,1 +1,59 @@
-let botonEncriptar = document.querySelector(".btn-encriptar");
+const textArea = document.querySelector("[name='text-area']");
+const mensaje = document.querySelector(".mensaje");
+
+function btnEncriptar() {
+	const textoEncriptado = encriptar(textArea.value);
+	mensaje.value = textoEncriptado;
+	textArea.value = "";
+	mensaje.style.backgroundImage = "none"; // quita la imagen
+}
+
+function encriptar(stringEncriptada) {
+	let matrizCodigo = [
+		["e", "enter"],
+		["i", "imes"],
+		["a", "ai"],
+		["o", "ober"],
+		["u", "ufat"],
+	];
+
+	stringEncriptada = stringEncriptada.toLowerCase();
+
+	for (let i = 0; i < matrizCodigo.length; i++) {
+		if (stringEncriptada.includes(matrizCodigo[i][0])) {
+			stringEncriptada = stringEncriptada.replaceAll(
+				matrizCodigo[i][0],
+				matrizCodigo[i][1]
+			);
+		}
+	}
+	return stringEncriptada;
+}
+
+function btnDesencriptar() {
+	const textoEncriptado = desencriptar(textArea.value);
+	mensaje.value = textoEncriptado;
+	textArea.value = "";
+	mensaje.style.backgroundImage = "none"; // quita la imagen
+}
+function desencriptar(stringDesencriptada) {
+	let matrizCodigo = [
+		["e", "enter"],
+		["i", "imes"],
+		["a", "ai"],
+		["o", "ober"],
+		["u", "ufat"],
+	];
+
+	stringDesencriptada = stringDesencriptada.toLowerCase();
+
+	for (let i = 0; i < matrizCodigo.length; i++) {
+		if (stringDesencriptada.includes(matrizCodigo[i][1])) {
+			stringDesencriptada = stringDesencriptada.replaceAll(
+				matrizCodigo[i][1],
+				matrizCodigo[i][0]
+			);
+		}
+	}
+	return stringDesencriptada;
+}
